0d21bf636f268a9c9af5b48ead564e9186c7909b
James Sullivan <james@git.git>
2015-02-13 23:34:05 -0500

    Basically all of the Schedule page

--- SproxWeb/controllers/scheduleController.js
+++ SproxWeb/controllers/scheduleController.js
@@ -4,6 +4,7 @@ sprox.controller('scheduleController',['$scope', '$location', '$timeout', functi
 	$scope.finals = []
 	var fiveminpx = 8;
 	var startTime = "8:00 AM";
+	var freeTimeTotal = 0;
 
 	if (userData.classesWeekly.Mo != "") $scope.weeklySchedule.push(dictWithDay(userData.classesWeekly.Mo));
 	if (userData.classesWeekly.Tu != "") $scope.weeklySchedule.push(dictWithDay(userData.classesWeekly.Tu));
@@ -12,53 +13,71 @@ sprox.controller('scheduleController',['$scope', '$location', '$timeout', functi
 	if (userData.classesWeekly.Fr != "") $scope.weeklySchedule.push(dictWithDay(userData.classesWeekly.Fr));
 
 	for (var day in $scope.weeklySchedule){
-		for(var curclass in $scope.weeklySchedule[day].classes){
-			var a = $scope.weeklySchedule[day];
+		var a = $scope.weeklySchedule[day];
+		a.displayName = getDayofWeek(parseInt(day));
+		console.log(getDayofWeek(day));
+
+		for(var curclass in a.classes){
 			var freeTime = DiffTime(startTime,a.classes[curclass].th_s);
 			var classDuration = DiffTime(a.classes[curclass].th_s,a.classes[curclass].th_e)
+			if (curclass != 0 && freeTime > 15){
+				console.log(freeTime);
+				freeTimeTotal = freeTimeTotal + freeTime;
+			}
 			a.classes[curclass].freeTime = freeTime;
 			a.classes[curclass].classDuration = classDuration;
+
 			startTime = a.classes[curclass].th_e;
 		}
+		a.endTime = startTime;
+		a.endTimePadding = DiffTime(startTime,"6:45 PM");
+		a.freeTimeTotal = freeTimeTotal;
+		freeTimeTotal = 0;
 		startTime = "8:00 AM";
 	}
 	
-
-	console.log(userData.classesWeekly);
-	console.log($scope.weeklySchedule);
 	$scope.finals = userData.finals;
 }]);
 
 function dictWithDay(dict, day) {
-	return {"day" : day, "classes" : dict}
+	return {"classes" : dict}
 }
+
 function getDayofWeek(i){
 	switch (i){
 		case 0:
-			return "Mo";
+			return "Monday";
 			break;
 		case 1:
-			return "Tu";
+			return "Tuesday";
 			break;
 		case 2:
-			return "We";
+			return "Wensday";
 			break;
 		case 3:
-			return "Th";
+			return "Thursday";
 			break;
 		case 4:
-			return "Fr";
+			return "Friday";
 			break;
 	}
 }
+
 function DiffTime(start, stop){
 		var tstart = new Date("02/08/14 " + start);
 		var tstop = new Date("02/08/14 " + stop)
-		console.log(tstop + " " + stop);
-		console.log(tstart + " " + start);
 		tstart = tstop - tstart;
-		console.log(tstart/60/60/1000);
 		return (tstart/60/1000)
 	
 }
 
+sprox.directive('scheduleSize', function($timeout) {
+    return {
+        link: function(scope, element, attr) {
+        	$timeout(function() {
+        		var size = attr.scheduleSize * 1.4
+        		element.css("height",size);
+        	}, 1);
+        }
+    };
+});
