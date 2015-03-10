3a866040246a7153ca789979f5bf6fd90b0c657d
Cameron Wheeler <cameron@scelos.com>
2015-03-09 23:22:46 -0400

    Started Card usage graph.

--- SproxNode/app/controllers/ucController.js
+++ SproxNode/app/controllers/ucController.js
@@ -2,6 +2,14 @@ sprox.controller('ucController',['$scope', '$location', '$timeout', function($sc
 	$scope.transactions = null;
 	$scope.loading = true;
 
+	$scope.usageLabels = [];
+	$scope.usageSeries = ['Series A', 'Series B'];
+
+	  $scope.usageData = [
+	    [65, 59, 80, 81, 56, 55, 40],
+	    [28, 48, 40, 19, 86, 27, 90]
+	  ];
+
 	$scope.checkFunds = function() {
 		if (funds !== 0) {
 			$scope.loading = false;
@@ -13,6 +21,13 @@ sprox.controller('ucController',['$scope', '$location', '$timeout', function($sc
 			$scope.dinningSwipes = funds[0].swipes;
 			$scope.guestSwipes = funds[0].guests;
 			$scope.transactions = funds[1];
+
+			for (var i = 0; i < $scope.transactions.length; i++) {
+				if ($scope.usageLabels.indexOf($scope.transactions[i].location) == -1) {
+					console.log($scope.transactions[i].location);
+					$scope.usageLabels.push($scope.transactions[i].location);
+				}
+			}
 
 			if ("CValue" == funds.mealPlanType) {
 				$scope.cvalue = false;
