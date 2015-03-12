ba29ff4f2c1986c79bc90a98672ec1713a4d0128
James Sullivan <james@git.git>
2015-03-11 23:39:12 -0400

    Made a much needed change to the Gruntfile and started stylizing schedule

--- SproxNode/app/controllers/scheduleController.js
+++ SproxNode/app/controllers/scheduleController.js
@@ -1,22 +1,22 @@
 sprox.controller('scheduleController',['$scope', '$location', '$timeout', function($scope, $location, $timeout) {
 	$scope.weeklySchedule = userData.classesWeekly;
-	$scope.colors =["#F44336", // Red
-					"#3F51B5", // Indigo
-					"#4CAF50", // Green
-					"#FF9800", // Orange
-					"#E91E63", // Pink
-					"#673AB7", // Deep Purple
-					"#2196F3", // Blue
-					"#009688", // Teal
-					"#8BC34A", // Light Green
-					"#FFEB3B", // Yellow
-					"#FF5722", // Deep Orange
-					"#9C27B0", // Purple
-					"#607D8B", // Blue Grey
-					"#00BCD4", // Cyan
-					"#FFC107", // Amber
-					"#CDDC39", // Lime
-					"#03A9F4"] // Light Blue
+	$scope.colors =["#E53935", // Red
+					"#1E88E5", // Blue
+					"#43A047", // Green
+					"#FB8C00", // Orange
+					"#D81B60", // Pink
+					"#5E35B1", // Deep Purple
+					"#3949AB", // Indigo
+					"#00897B", // Teal
+					"#7CB342", // Light Green
+					"#FDD835", // Yellow
+					"#F4511E", // Deep Orange
+					"#8E24AA", // Purple
+					"#546E7A", // Blue Grey
+					"#00ACC1", // Cyan
+					"#FFB300", // Amber
+					"#C0CA33", // Lime
+					"#039BE5"] // Light Blue
 
 	//We Don't Support Weekend Classes, if that even happens.
 	$scope.finals = userData.finals;
