fe505d7fc0d7952fe597f49bc68a71fcc78c885a
Cameron Wheeler <cameron@scelos.com>
2015-09-10 15:21:07 -0400

    Started crude dining to JSON parsing

--- SproxNode/serviceConnectors/diningConnector.js
+++ SproxNode/serviceConnectors/diningConnector.js
@@ -18,12 +18,35 @@ module.exports = function(user,passwd) {
 	    	rawMenu = rawMenu.replace(/Today's Menu/g, "");
 
 	    	var lines = rawMenu.split("\n");
+	    	var curTime;
+	    	var menu = {};
+
 	    	for (var i = 0; i < lines.length; i++) {
-	    		if (lines[i].trim() != "" && lines[i].indexOf("SPECIAL") == -1) {
-	    			this.emit("console", "'" + lines[i].trim() + "'");
+	    		var line = lines[i].trim();
+	    		if (line != "" && line.indexOf("SPECIAL") == -1) {
+	    			if (line == "Breakfast" || line == "Lunch" || line == "Dinner Menu") {
+	    				curTime = line.replace(" ","").replace("Menu", "");
+ 						menu[curTime] = [];
+	    			} else if (line.indexOf("The dining area is divided") > -1) {
+	    				i += 4;
+	    			} else if (line.indexOf("Entrée") < 0 && line.indexOf("UMass Bakeshop") < 0) {
+	    				var loc_ex = /\(.*?\)/g;
+	    				var locs = loc_ex.exec(lines[i].trim());
+	    				var item = lines[i].trim().replace("w/", "with ").replace(/\s\s+/g, " ");
+
+	    				if (locs != null) {
+	    					locs = locs[0].replace("(","").replace(")","").split(" ");
+	    				//	item = item.replace(locs[0].toString(), "");
+	    				} else {
+	    					locs = "all";
+	    				}
+	    				
+	    				menu[curTime].push({item : item, locations : locs});
+	    			}
 	    		}
-	    		
 	    	}
+
+	    	this.emit("console", menu);
 	    });
 
 		spooky.run();	
